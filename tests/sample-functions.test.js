70cfa7074e429ac006dc33821ed2547f0194cd24
unknown <jakanji@calpoly.edu>
2026-01-26 20:48:00 -0800

    added more tests

--- tests/sample-functions.test.js
+++ tests/sample-functions.test.js
@@ -1,35 +1,44 @@
 const myFunctions = require('./sample-functions.js');
 
-test('basic div test', () =>{
+test('testing div --true', () =>{
     const result = myFunctions.div(9, 3);
     const target = 3;
     expect(result).toBe(target);
 })
 
-test('div by zero', () => {
+test('testing div -- by zero', () => {
     expect(myFunctions.div(10,0)).toBeUndefined;
 })
 
-test('div by negative', () => {
+test('testing div -- by negative', () => {
     expect(myFunctions.div(2, -1)).toBe(-2)
 })
 
-test('div by larger', () => {
+test('testing div -- by larger', () => {
     expect(myFunctions.div(2, 10)).toBe(0.2)
 })
 
-test('containNumbers false', () => {
+test('testing containNumbers -- false', () => {
     expect(myFunctions.containsNumbers("text")).toBe(false);
 })
 
-test('test first char number', () => {
+test('testing contiansNubmers -- first char number', () => {
     expect(myFunctions.containsNumbers("1text")).toBe(true);
 })
 
-test('testing 2nd char number', () => {
+test('testing contiansNubmers -- 2nd char number', () => {
     expect(myFunctions.containsNumbers("t1ext")).toBe(true);
 })
 
-test('testing 2nd char number', () => {
+test('testing contiansNubmers -- last char number', () => {
     expect(myFunctions.containsNumbers("text1")).toBe(true);
 })
+
+test('testing containsNumbers -- whitespaces', () => {
+    expect(myFunctions.containsNumbers("text text")).toBe(false);
+})
+
+test('testing containsNumbers -- whitespaces w/ number', () => {
+    expect(myFunctions.containsNumbers("text 1 text")).toBe(true);
+})
+
